969762cd63b2ad5c2787fc5d09da50e3816d8f28
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2024-01-29 16:32:46 +0530

    fix: Update Hardhat

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  const PROXY_ADDRESS = "0x489F56e3144FF03A887305839bBCD20FF767d3d1";
+  const PROXY_ADDRESS = "0x4A598B7eC77b1562AD0dF7dc64a162695cE4c78A";
   // forceImport is used to force the import of the contract if it is not present in .openzeppelin folder
   const forceImport = false;
 
@@ -9,16 +9,22 @@ async function main() {
     "TransakMulticallExecuter"
   );
 
-  // if (true) {
-  //   await upgrades.forceImport(PROXY_ADDRESS, TransakMulticallExecuter);
-  //   console.log("TransakMulticallExecuter imported");
-  // }
+  if (forceImport) {
+    await upgrades.forceImport(PROXY_ADDRESS, TransakMulticallExecuter);
+    console.log("TransakMulticallExecuter imported");
+  }
 
   const transakMulticallExecuter = await upgrades.upgradeProxy(
     PROXY_ADDRESS,
-    TransakMulticallExecuter
+    TransakMulticallExecuter,
+    {
+      redeployImplementation: "always",
+    }
   );
-  console.log("TransakMulticallExecuter upgraded",await  transakMulticallExecuter.getAddress());
+  console.log(
+    "TransakMulticallExecuter upgraded",
+    await transakMulticallExecuter.getAddress()
+  );
 }
 
 main();
